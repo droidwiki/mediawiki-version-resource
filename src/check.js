cb1eb76526619ffd6282866119a518615872cc3d
Florian <florian.schmidt.welzow@t-online.de>
2019-02-17 17:36:57 +0100

    Initial commit for check script

---
+++ src/check.js
@@ -0,0 +1,62 @@
+import * as http from "http";
+import * as https from "https";
+import * as readline from "readline";
+
+const apiRequestPath = '/w/api.php?action=query&meta=siteinfo&siprop=general&formatversion=2&format=json';
+
+function client(url) {
+    if (url.toString().indexOf("https") === 0){
+        return https;
+    }
+    return http;
+}
+
+export function check(stdin) {
+    const rl = readline.createInterface({
+        input: stdin,
+        crlfDelay: Infinity
+    });
+
+    return new Promise((resolve, reject) => {
+        rl.on('line', (line) => {
+            const input = JSON.parse(line);
+            if (!input.source.mediawikiUrl) {
+                reject('The mediawiki URL is a required source config.');
+                return;
+            }
+
+            const discoveredVersions = [];
+            if (input.version) {
+                discoveredVersions.push(input.version);
+            }
+            const url = new URL(apiRequestPath, new URL(input.source.mediawikiUrl));
+            client(url).get(url.toString(), (res) => {
+                if (res.statusCode !== 200) {
+                    process.stdout.write(JSON.stringify(discoveredVersions));
+                    reject();
+                    return;
+                }
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    const apiResponse = JSON.parse(data);
+                    if (!apiResponse.query.general['git-branch']) {
+                        process.stdout.write(JSON.stringify([]));
+                        resolve();
+                    }
+                    const version = apiResponse.query.general['git-branch'];
+                    if (!discoveredVersions.includes(version)) {
+                        discoveredVersions.push(version);
+                    }
+                    process.stdout.write(JSON.stringify(discoveredVersions));
+                    resolve();
+                });
+            }).on('error', (error) => {
+                reject(error);
+            });
+        });
+
+    });
+}
---
+++ src/check.spec.js
@@ -0,0 +1,85 @@
+import {check} from "./check";
+import * as http from "http";
+import * as stream from "stream";
+
+describe('check script', () => {
+    let server;
+    let port;
+    let stdin;
+    let response;
+
+    beforeEach((done) => {
+        stdin = new stream.Readable();
+        stdin._read = f => f;
+        server = http.createServer((req, res) => {
+            expect(req.url).toBe('/w/api.php?action=query&meta=siteinfo&siprop=general&formatversion=2&format=json');
+            res.write(response);
+            res.end();
+        });
+        server.listen(0, () => {
+            port = server.address().port;
+            done();
+        });
+    });
+
+    afterEach(() => {
+        server.close();
+    });
+
+    it('should throw error on missing mediawiki URL', (done) => {
+        check(stdin)
+            .then(fail)
+            .catch((error) => {
+                expect(error).toEqual('The mediawiki URL is a required source config.');
+                done();
+            });
+
+        stdin.push('{"source": {}}\n');
+    });
+
+    it('should return current version if no new versions are available', (done) => {
+        response = JSON.stringify({"batchcomplete": true, "query": {"general": {"git-branch": "wmf/1.33.0-wmf.17"}}});
+        process.stdout.write = jest.fn();
+
+        check(stdin).then(() => {
+            expect(process.stdout.write).toHaveBeenCalledWith('["wmf/1.33.0-wmf.17"]');
+            done();
+        });
+
+        stdin.push('{"version": "wmf/1.33.0-wmf.17", "source": {"mediawikiUrl": "http://localhost:' + port + '"}}\n');
+    });
+
+    it('should return empty list if no versions available', (done) => {
+        response = JSON.stringify({"batchcomplete": true, "query": {"general": {}}});
+        process.stdout.write = jest.fn();
+
+        check(stdin).then(() => {
+            expect(process.stdout.write).toHaveBeenCalledWith('[]');
+            done();
+        }).catch(fail);
+
+        stdin.push('{"source": {"mediawikiUrl": "http://localhost:' + port + '"}}\n');
+    });
+
+    it('should return new discovered version', (done) => {
+        response = JSON.stringify({"batchcomplete": true, "query": {"general": {"git-branch": "wmf/1.33.0-wmf.18"}}});
+
+        check(stdin).then(() => {
+            expect(process.stdout.write).toHaveBeenCalledWith('["wmf/1.33.0-wmf.18"]');
+            done();
+        });
+
+        stdin.push('{"source": {"mediawikiUrl": "http://localhost:' + port + '"}}\n');
+    });
+
+    it('should reject on error', (done) => {
+        check(stdin)
+            .then(fail)
+            .catch((error) => {
+                expect(error.message).toEqual('getaddrinfo ENOTFOUND an_url an_url:80');
+                done();
+            });
+
+        stdin.push('{"source": {"mediawikiUrl": "http://AN_URL"}}\n');
+    });
+});
